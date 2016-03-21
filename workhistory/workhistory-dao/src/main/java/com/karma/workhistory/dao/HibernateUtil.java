bede7f099aeb29a803fdff2b333238d7d091d487
pradeep S <mathrupradeep@gmail.com>
2016-03-21 19:37:40 +0530

    Adding the hibernateUtilFile

--- workhistory/workhistory-dao/src/main/java/com/karma/workhistory/dao/HibernateUtil.java
+++ workhistory/workhistory-dao/src/main/java/com/karma/workhistory/dao/HibernateUtil.java
@@ -2,34 +2,15 @@ package com.karma.workhistory.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory = buildSessionFactory();
-    private static String filePath = "";
+	@Autowired
+	private static SessionFactory sessionFactory;
 
-    private static SessionFactory buildSessionFactory() {
-        try {
-            if (sessionFactory == null) {
-                sessionFactory = new Configuration().configure(filePath).buildSessionFactory();
-            }
-            return sessionFactory;
-        } catch (final Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed."
-                    + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    // for junits
-    public HibernateUtil(final String filePath) {
-        HibernateUtil.filePath = filePath;
-    }
-
-    protected Session getSession() {
-        return sessionFactory.openSession();
-    }
+	protected Session getSession() {
+		return sessionFactory.openSession();
+	}
 
 }
