2518492b27301ffd8595985062b80c406115b6e4
pradeep S <mathrupradeep@gmail.com>
2016-03-21 19:33:53 +0530

    Added the working model of the project

--- workhistory/workhistory-model/src/main/java/com/karma/workhistory/model/Transaction.java
+++ workhistory/workhistory-model/src/main/java/com/karma/workhistory/model/Transaction.java
@@ -2,7 +2,9 @@ package com.karma.workhistory.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -13,5 +15,11 @@ public class Transaction implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 268870749754336868L;
+	
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	
 
 }
