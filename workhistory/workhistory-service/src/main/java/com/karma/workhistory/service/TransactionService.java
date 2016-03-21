c19b633906553c97e9741de9d5a8856b89065572
pradeep S <mathrupradeep@gmail.com>
2016-03-21 20:21:18 +0530

    Adding Transaction service and tx-annotation in datasource.xml

---
+++ workhistory/workhistory-service/src/main/java/com/karma/workhistory/service/TransactionService.java
@@ -0,0 +1,11 @@
+package com.karma.workhistory.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class TransactionService {
+
+}
