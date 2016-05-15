0dbf238ba349a0476c52228e2d8a915147da946e
pradeep S <mathrupradeep@gmail.com>
2016-05-15 12:01:46 +0530

    Implented password hashing

--- workhistory/workhistory-service/src/main/java/com/karma/workhistory/service/util/RandomPasswordGenerator.java
+++ workhistory/workhistory-service/src/main/java/com/karma/workhistory/service/util/RandomPasswordGenerator.java
@@ -1,5 +1,7 @@
 package com.karma.workhistory.service.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class RandomPasswordGenerator {
@@ -11,6 +13,26 @@ public class RandomPasswordGenerator {
 		StringBuilder builder = new StringBuilder(len);
 		for (int i = 0; i < len; i++)
 			builder.append(passwordCharacters.charAt(random.nextInt(passwordCharacters.length())));
-		return builder.toString();
+		return generateHash(builder.toString());
+	}
+	
+	public static String generateHash(String input) {
+		StringBuilder hash = new StringBuilder();
+
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			byte[] hashedBytes = sha.digest(input.getBytes());
+			char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+					'a', 'b', 'c', 'd', 'e', 'f' };
+			for (int idx = 0; idx < hashedBytes.length; ++idx) {
+				byte b = hashedBytes[idx];
+				hash.append(digits[(b & 0xf0) >> 4]);
+				hash.append(digits[b & 0x0f]);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// handle error here.
+		}
+
+		return hash.toString();
 	}
 }
